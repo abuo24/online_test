4c4f9019fffa5ea3bd2a9a6c6fbd070c9f713075
Orifjon Yunusjonov <mryediniofficial9924@gmail.com>
2021-05-06 00:52:42 +0500

    vgvjvfs

--- src/main/java/com/example/online_test/entity/User.java
+++ src/main/java/com/example/online_test/entity/User.java
@@ -39,7 +39,7 @@ public class User implements Serializable {
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique=true)
     private String phoneNumber;
 
     @ManyToOne(fetch = FetchType.LAZY)
